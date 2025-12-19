a2f7225113115dbfd0fe0b84e104f90ccc5aeb5a
Alan K Joseph <alankjoseph29@gmail.com>
2025-12-19 13:58:42 +0530

    Change DB password to placeholder for security
    Updated database password placeholder for security.

--- src/main/java/com/app/student/DBConnection.java
+++ src/main/java/com/app/student/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
 	private static final String URL="jdbc:mysql://localhost:3306/student_db_jdbc";
 	private static final String USER="root";
-	private static final String PASSWORD="Root@123";
+	private static final String PASSWORD="YOUR_DB_PASSWORD";// Set your MySQL password here (not committed for security reasons)
 	
 	private DBConnection() {}
 	
